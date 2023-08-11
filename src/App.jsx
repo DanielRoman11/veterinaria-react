a79f9f867d03f64b2cede5929573e3fccaaa6f10
danielroman11 <danielroman.05111@gmail.com>
2023-08-10 23:18:23 -0500

    adding react developer tools

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
 
   return (
     <>
-      <Header></Header>
-      <Formulario></Formulario>
-      <ListadoPacientes></ListadoPacientes>
+      <Header />
+      <Formulario />
+      <ListadoPacientes /> 
     </>
   )
 }
