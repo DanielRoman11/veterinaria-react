42d8ab5894c4690ae232e59ef8fe4e57744519d2
danielroman11 <danielroman.05111@gmail.com>
2023-09-17 19:35:33 -0500

    feat: UseState Formulario

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,13 @@ import ListadoPacientes from "./components/ListadoPacientes";
 function App() {
   return (
     <>
-      <div className="min-[300px]:px-2 container mx-auto mt-20 ">
+      <main className="container mx-auto">
         <Header />
         <div className="sm:flex justify-center gap-2 mt-12">
           <Formulario />
           <ListadoPacientes /> 
-
         </div>
-      </div>
+      </main>
     </>
   )
 }
